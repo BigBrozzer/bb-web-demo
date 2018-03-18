895b052b3b9ef12161bf96392effdef56d0593ce
Oleksandr Blyznuk <blyzniuk.oleksandr@gmail.com>
2018-03-18 10:58:43 +0200

    reinit

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import {applyMiddleware, createStore} from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware';
-import { bbMiddleware, bbMLabProvider } from 'big-brother';
+import { bbConnector, bbMiddleware, bbMLabProvider } from 'big-brother';
 
 import rootReducer from './reducers/index';
 
@@ -14,14 +14,15 @@ const defaultState = {
     }
 };
 
-const journeyMiddleware = bbMiddleware(rootReducer, bbMLabProvider({
+const middleware = applyMiddleware(bbMiddleware, thunk, logger(), promise());
+const store = createStore(rootReducer, defaultState, middleware);
+
+const apiProvider = bbMLabProvider({
     dbName: 'my-package',
     collName: 'journey',
     apiKey: 'Orf_sZpA2Pp2O5JdEoVUOZqq5dcRpeO5',
-}));
-
-const middleware = applyMiddleware(journeyMiddleware, thunk, logger(), promise());
-const store = createStore(rootReducer, defaultState, middleware);
+});
+bbConnector(store, rootReducer, apiProvider);
 
 if (module.hot) {
     module.hot.accept('./reducers/', () => {
